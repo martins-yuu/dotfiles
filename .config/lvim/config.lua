d4ce61aff89b5ad2676bb02064d79f9df93510b0
Yuuko Martins <yuukomartins@pm.me>
2023-12-27 19:57:41 -0300

    set up copilot

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -13,7 +13,17 @@ lvim.plugins = {
     "Pocco81/auto-save.nvim",
     config = function()
       require("auto-save").setup()
-    end
+    end,
   },
-  { "github/copilot.vim" },
+  {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()
+        require("copilot_cmp").setup()
+      end, 100)
+    end,
+  },
 }
