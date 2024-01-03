589bfe0c91a45e3333ad5f033be702c55ddf76f0
Yuuko Martins <yuukomartins@pm.me>
2024-01-03 15:11:03 -0300

    set up smooth scrolling

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -16,6 +16,23 @@ lvim.plugins = {
     end,
   },
   {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require("neoscroll").setup({
+        mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+        hide_cursor = true,
+        stop_eof = true,
+        use_local_scrolloff = false,
+        respect_scrolloff = false,
+        cursor_scrolls_alone = true,
+        easing_function = nil,
+        pre_hook = nil,
+        post_hook = nil,
+      })
+    end,
+  },
+  {
     "zbirenbaum/copilot-cmp",
     event = "InsertEnter",
     dependencies = { "zbirenbaum/copilot.lua" },
