f539dc41e2052761855717e7e7e29bbbf7d7cad8
Yuuko Martins <yuukomartins@pm.me>
2024-04-23 06:16:24 -0300

    Initial commit

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,21 @@
+# Config
+alias zconfig='$EDITOR $HOME/.zshrc'
+
+# Shell
+alias cd="z"
+alias ls="eza --icons"
+alias cat="bat"
+alias grep="rg"
+alias less="moar"
+alias cl="clear"
+
+# Arch
+alias pi="yay -S"
+alias pu="yay -Syu"
+alias pr="sudo pacman -Rns"
+alias pc="sudo pacman -Scc"
+
+# Misc
+alias c="code -r"
+alias nf="clear && neofetch"
+alias names="names | tr '-' '_'"
